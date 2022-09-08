1310b04c1c89ff69078eb90ba82e784cb68a1c08
pouya16 <pouya16@gmail.com>
2022-09-08 05:55:36 -0700

    1400/06/17 second attack to our custom widget ... many kotlin

---
+++ app/src/main/java/com/example/wallebi_app/custom_widgets/p_text_Inputs/WallebiInput.kt
@@ -0,0 +1,50 @@
+package com.example.wallebi_app.custom_widgets.p_text_Inputs
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.text.Layout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.example.wallebi_app.R
+
+
+class WallebiInput @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+    defStyleRes: Int=0
+) : LinearLayout(context,attrs,defStyle,defStyleRes){
+    init {
+        LayoutInflater.from(context).inflate(R.layout.p_layout_input,this,true)
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it,R.styleable.wallebi_input,0,0)
+
+        }
+    }
+}
+
+/*
+class WallebiInput : LinearLayout {
+
+
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+    )
+            : super(context, attrs, defStyleAttr)
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    )
+            : super(context, attrs, defStyleAttr, defStyleRes)
+}
+*/
+
