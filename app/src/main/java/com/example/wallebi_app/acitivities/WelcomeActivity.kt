3b77dd87a333b00534ab97aa083c0afaa435dad2
pouya16 <pouya16@gmail.com>
2022-10-02 09:12:53 -0700

    1401/07/10 end login signup designs

---
+++ app/src/main/java/com/example/wallebi_app/acitivities/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wallebi_app.acitivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallebi_app.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
