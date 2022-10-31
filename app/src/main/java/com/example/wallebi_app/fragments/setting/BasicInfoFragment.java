c72b0db1edd40db083f6557b70598a2c7e4f891c
Pouya <pouya@Pouyas-MacBook-Air.local>
2022-10-31 05:03:00 -0700

    1401/08/09 setting and homepage ui

---
+++ app/src/main/java/com/example/wallebi_app/fragments/setting/BasicInfoFragment.java
@@ -0,0 +1,26 @@
+package com.example.wallebi_app.fragments.setting;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wallebi_app.R;
+
+public class BasicInfoFragment extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_basic_info, container, false);
+
+        return view;
+    }
+}
